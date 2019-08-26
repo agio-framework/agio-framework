c1fdf67e5824bd288a48adf61de84f1979e34ea8
Tales Luna <tales.ferreira.luna@gmail.com>
2019-08-26 02:30:30 -0300

    fix: new app path resolution strategy

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,18 +1,27 @@
-global.APP_PATH = (require.main as any).path;
+import { dirname } from 'path';
 
+// Declare globals
+global.__agio__ = {};
+global.APP_PATH = dirname(require.main.filename);
+
+import { environment } from '@agio/framework/environment';
 import express, { Application } from 'express';
 import { HANDLERS } from '@agio/framework/common';
-import { environment } from '@agio/framework/environment';
 import { Http2Server } from 'http2';
 import { createServer } from 'https';
 import { readFileSync } from 'fs';
 import { AppOptions } from '@agio/framework/core';
 import { Database } from '@agio/framework/database';
 
+/**
+ * Agio main App
+ */
 export class App {
-
+    
     private instance = express();
     private options: AppOptions;
+    private readonly HOST = 'localhost';
+    private readonly PORT = 8889;
 
     constructor(options?: AppOptions) {
         this.options = options;
@@ -22,12 +31,10 @@ export class App {
 
         try {
 
-            const {port, host, ssl} = {
-                host: 'localhost',
-                port: 8889,
-                ...environment.server
-            };
+            // Get environemnt server confs, use HOST and PORT default if they not passed in env
+            const {port, host, ssl} = { host: this.HOST, port: this.PORT, ...environment.server};
     
+            // Server
             let server: Http2Server | Application = this.instance;
             let protocol = 'http';
     
@@ -52,6 +59,10 @@ export class App {
 
     });
 
+
+    /**
+     * Start application
+     */
     public start() {
 
         this.syncDatabases();
@@ -61,15 +72,17 @@ export class App {
 
     }
 
+
+    /**
+     * Import routes and controllers
+     */
     private route() {
 
         // Setup custom handlers
         if (this.options.handlers) this.options.handlers.forEach(handle => HANDLERS[handle.at][handle.provide as any] = handle.use);
 
-
         // Use before handlers
         this.instance.use(Object.keys(HANDLERS.before).map(key => HANDLERS.before[key]));
-
 
         // Use controllers
         if (this.options.controllers) {
@@ -80,18 +93,24 @@ export class App {
 
         }
 
-
         // Use after handlers
         this.instance.use(Object.keys(HANDLERS.after).map(key => HANDLERS.after[key]));
 
     }
 
+    
+    /**
+     * Sync defined databases;
+     */
     private syncDatabases() {
         
+        // Environment databases list
         const { databases } = environment;
 
+        // Database list not defined
         if (!databases) return;
 
+        // Database list defined, sync this.
         Object.keys(databases).forEach(dbName => {
 
             const database = databases[dbName];
@@ -101,7 +120,7 @@ export class App {
 
             new Database(uri, dbName, options);
 
-        })
+        });
 
     }
 
